210c474ffd85238a12f77468732c16657853157c
guyavraham <guy1812@gmail.com>
2022-02-09 15:58:30 +0200

    delete

--- src/api/apiConfig.js
+++
@@ -1,7 +0,0 @@
-export const apiConfig = {
-  baseUrl: "https://pokeapi.co/api/v2/",
-};
-
-export const endPoints = {
-  allPokemons: "pokemon?limit=12",
-};
--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import pokemonReducer from "./reducers/pokemonReducer";
-const store = configureStore({
-  reducer: {
-    pokemons: pokemonReducer,
-  },
-});
-
-export default store;
